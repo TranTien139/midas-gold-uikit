c950f3d45783f267e6c8e510e680b8a24a77b1d4
tien.tran <tien.tran@midasprotocol.io>
2021-02-23 14:10:03 +0700

    update build

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -28,8 +28,9 @@ const StyledNav = styled.nav<{ showMenu: boolean }>`
   padding-right: 16px;
   width: 100%;
   height: ${MENU_HEIGHT}px;
-  background-color: ${({ theme }) => theme.nav.background};
-  border-bottom: solid 2px rgba(133, 133, 133, 0.1);
+  // background-color: ${({ theme }) => theme.nav.background};
+  background-color: #18181A;
+  border-bottom: solid 2px #fff;
   z-index: 20;
   transform: translate3d(0, 0, 0);
 `;
